cf6c8da42278ad1f5fd8ca2a42f4ff1cc9fc8e00
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-23 21:49:14 -0400

    non-working version using protractor

--- sample-scenario.js
+++ sample-scenario.js
@@ -1,6 +1,7 @@
 var webdriver = require('selenium-webdriver');
 var assert = require('assert');
 var util = require('util');
+var protractor = require('protractor');
 
 var driver = new webdriver.Builder().
     usingServer('http://localhost:4444/wd/hub').
@@ -10,3 +11,16 @@ var driver = new webdriver.Builder().
       'platform': 'ANY',
       'javascriptEnabled': true
     }).build();
+
+var ptor = protractor.wrapDriver(driver);
+
+driver.manage().timeouts().setScriptTimeout(10000);
+
+ptor.get('http://localhost:3000/testApp/');
+
+var message = ptor.findElement(protractor.By.binding('{{serverMessage}}'));
+message.getText().then(function(text) {
+  assert.equal('This is the response.', text);
+});
+
+driver.quit();
