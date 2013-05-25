ece87c144186d87e45d89dc9b353ae96f3b7479d
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-05-24 20:31:48 -0400

    add another view. Add bootstrap

--- testApp/app.js
+++ testApp/app.js
@@ -1,10 +1,15 @@
 var app = angular.module('SampleApp', []);
 
 app.config(function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'partial.html',
-    controller: 'SampleCtrl'
-  })
+  $routeProvider
+      .when('/', {
+        templateUrl: 'home.html',
+        controller: 'SampleCtrl'
+      })
+      .when('/other-view', {
+        templateUrl: 'other.html',
+        controller: 'OtherCtrl'
+      })
 });
 
 app.controller('SampleCtrl', function($scope, $http) {
@@ -12,5 +17,13 @@ app.controller('SampleCtrl', function($scope, $http) {
 
   $http({method: 'GET', url: '/message'}).success(function(data) {
     $scope.serverMessage = data;
-  })
+  });
+
+  $scope.showMeMessage = function() {
+    $scope.message = 'Greetings';
+  };
+});
+
+app.controller('OtherCtrl', function($scope) {
+  $scope.msg = 'The other view';
 });
